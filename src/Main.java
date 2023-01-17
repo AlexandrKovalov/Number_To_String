25a57b10a627a9c7331e2ab1e6045537dc5b58fe
AlexandrKovalov <sashko592@gmail.com>
2023-01-17 19:56:16 +0300

    Readu to use

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+class Main {
+    public static void main(String[] args) {
+        System.out.println(numberToString(4586));
+    }
+    public static String numberToString(int num) {
+       return "" + num;
+    }
+}
